eed481bbe05abdbbafc7391acc854130f5c24b22
younusraza909 <younusraza909@gmail.com>
2023-08-04 17:13:42 +0500

    game completed

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Element Selection
 const cells = document.querySelectorAll(".cell");
+const summary = document.querySelector(".summary");
+const restartBtn = document.querySelector(".restart");
+
+const overlay = document.querySelector(".overlay");
 
 // This is an array whcih contain all possible combination of winning
 const winningCombination = [
@@ -56,24 +60,53 @@ function isDraw() {
   return gameState.length === 9 && !gameState.includes(undefined);
 }
 
+function showSummaryText(text) {
+  const p = document.createElement("p");
+
+  p.innerText = text;
+
+  // clearing inner html for summary
+  summary.innerHTML = "";
+  summary.appendChild(p);
+
+  // Showing overlay
+  overlay.classList.remove("none");
+}
+
+// Restart Game
+function restartGame() {
+  gameState.length = 0;
+  playerTurn = PLAYER_1;
+  overlay.classList.add("none");
+
+  // We have to remove classes from all div of cross and circle
+  cells.forEach((cell) => {
+    cell.classList = ["cell"];
+  });
+}
+
 //  Adding event listner on cell
 cells.forEach((cell) => {
   cell.addEventListener("click", () => {
     if (checkCell(cell)) {
       markCell(cell);
-
       // Check after making current player move is he/she won the game
       if (checkWin()) {
         // State if any user win
-        console.log(`${playerTurn} Wins`);
+        showSummaryText(`${playerTurn} Wins`);
+
         return;
       }
-
       if (isDraw()) {
-        console.log("Game was drawn");
-      }
+        showSummaryText("Game was drawn");
 
+        return;
+      }
       switchPlayer();
     }
   });
+});
+
+restartBtn.addEventListener("click", () => {
+  restartGame();
 });
